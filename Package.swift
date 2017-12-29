fef97b7c0b73238f28546b8969a79af9e0ff3f82
Vrisch <magnus@vrischikasana.se>
2017-12-29 16:24:31 +0100

    Removed Orbit dependency

--- Package.swift
+++ Package.swift
@@ -6,14 +6,10 @@ let package = Package(
     products: [
         .library(
             name: "Sunflare",
-            targets: ["Sunflare"]),
-        ],
-    dependencies: [
-        .package(url: "https://github.com/vrisch/Orbit.git", .branch("develop")),
+            targets: ["Sunflare"])
         ],
     targets: [
         .target(
-            name: "Sunflare",
-            dependencies: ["Orbit"]),
+            name: "Sunflare")
         ]
 )
